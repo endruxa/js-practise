13dc0074a6ceea538679a1f12349601196fe4843
Endru <nesterov.andrey.a@gmail.com>
2018-08-07 21:03:07 +0300

    s

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,44 @@
+//перемещение блока div
+body.addEventListener('click', function (event) {
+    if (event.ctrlKey) {
+        console.log(event.offsetX, ' ', event.offsetY)
+        menu.style.left = ( (event.pageX >= 25) ? event.pageX - 25 : 0) + 'px';
+        menu.style.top = ( (event.pageY >= 25) ? event.pageY - 25 : 0) + 'px';
+    }
+});
+
+
+
+
+/*body.addEventListener('keypress', function (event) {
+    console.log(event.target)
+})
+
+
+dd1.onclick = function (e) {
+    e.stopPropagation();
+};
+
+
+body.addEventListener('oncopy', function (event) {
+    var clipboard = (event || window.event).clipboardData;
+    if (clipboard){
+        console.log('Произошло копирование');
+    }
+    //console.log(event.charCode);
+});
+
+
+body.addEventListener('click', function (event) {
+    if (event.altKey && event.ctrlKey) {
+        console.log(event.x, '', event.y)
+    } else {
+        console.log(event.offsetX, '', event.offsetY)
+    }
+});*/
+
+
+/*
 var p = document.getElementById("id1");
 p.addEventListener("click", clickP);
 var k = 0,
@@ -18,4 +59,5 @@ function clickP(event) {
 	}
 	
 var d = document.getElementsByTagName('p');
+*/
 
