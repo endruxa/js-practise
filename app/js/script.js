9c81512c18dc559606e216d15132fa8e26eedbfc
endruxa <nesterov.andrey.a@.com>
2018-08-09 21:42:52 +0300

    s

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,55 @@
-//перемещение блока div
-body.addEventListener('click', function (event) {
+var array = $('').is(function () {
+	console.log($(this).eq(0).);
+	return true;
+});
+
+//array = array.filter(':contains("hello")');
+for (var i = 0; i < array.length; i++) {
+	//array.eq(i).css('display', 'none');
+	array.eq(i).css('font-size', (10 * (i + 1)) + 'px')
+	array.eq(i).css('color', 'red')
+}
+/*var array = $('p').each(function (index, elem) {
+	elem.style = 'font-size:' + (10 * (index + 1)) + 'px'
+});*/
+/*for (var i = 0; i < array.length; i++) {
+	array.eq(i).css('font-size', (10 * (i + 1)) + 'px')
+	array.eq(i).css('color', 'red')
+	array.eq(i).prop("disabled", false)
+}*/
+
+/*var array = $(':disabled');
+for (var i = 0; i < array.length; i++) {
+	array.eq(i).css('font-size', (10 * (i + 1)) + 'px')
+	array.eq(i).css('color', 'red')
+	array.eq(i).prop("disabled", false)
+}*/
+/*var array = $('p:contains("hello")');//(' + 'hello' + ')
+console.log(array.slice(1, 4));
+for (var i = 0; i < array.length; i++) {
+	array.eq(i).css('font-size', (10 * (i + 1)) + 'px')
+	array.eq(i).css('color', 'red')
+}*/
+
+
+//console.log($('.data .tab'))//селектор по классу
+
+/*
+console.log($('p').eq(0).css('font-size', '40px'));
+$('p').eq(1).css('font-size', '30px');
+//перебор всех p и увеличение размера шрифта
+var array = $('body div p');// ('p')
+	for (var i = 0; i < array.length; i++) {
+		array.eq(i).css('font-size', (10 * (i + 1)) + 'px')
+}
+*/
+
+//console.log($('div.data').css('background', 'blue'));
+
+//jquery выбор всех элементов страницы
+//console.log($('*'));
+
+/*body.addEventListener('click', function (event) {
     if (event.ctrlKey) {
         console.log(event.offsetX, ' ', event.offsetY)
         menu.style.left = ( (event.pageX >= 25) ? event.pageX - 25 : 0) + 'px';
@@ -8,9 +58,7 @@ body.addEventListener('click', function (event) {
 });
 
 
-
-
-/*body.addEventListener('keypress', function (event) {
+body.addEventListener('keypress', function (event) {
     console.log(event.target)
 })
 
@@ -35,11 +83,10 @@ body.addEventListener('click', function (event) {
     } else {
         console.log(event.offsetX, '', event.offsetY)
     }
-});*/
+});
+*/
 
-
-/*
-var p = document.getElementById("id1");
+/*var p = document.getElementById("id1");
 p.addEventListener("click", clickP);
 var k = 0,
 	n = document.getElementById('dd');
